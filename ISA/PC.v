7c49415f9ac86762896f3417f0fa1991fb27add8
mrn071 <mohammadrezanaziri0@gmail.com>
2023-07-04 15:21:38 +0330

    PC module added

--- ISA/PC.v
+++ ISA/PC.v
@@ -19,7 +19,31 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module PC(
+    input clk,
+    input [15:0] oldPC,
+    input PCSrc,
+    input zero,
+    input branch,
+    input [15:0] ExtBrnImm,
+    input [12:0] absjmp,
+    output reg [15:0] Pc
     );
+
+    always @(posedge clk) begin
+        case(PCSrc)
+            2'b10:begin //jmp
+                Pc[12:0] = absjmp;
+                Pc = Pc << 1;
+                Pc[15:14] = oldPC[15:14];
+            end
+            2'b00:begin
+                if(zero & branch)
+                    Pc = oldPC + 2 + ExtBrnImm;
+                else
+                    Pc = Pc + 2;
+            end
+        endcase
+    end
 
 
 endmodule
